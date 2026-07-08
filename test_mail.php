e6c4553def61aece72e585c43880c635bfed38b9
Tijani Moro <tjhackx111@gmail.com>
2026-07-08 03:49:06 +0000

    clean up

--- test_mail.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/mailer.php';
-
-echo "Testing mailer...\n";
-$result = sendMail('alerts@qwiktransfers.com', 'Test Email', 'This is a test email');
-var_dump($result);
